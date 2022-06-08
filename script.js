03e53826037f85e8df3d7c27025201ffdb5ec147
meganbeek98 <meganbeek98@gmail.com>
2022-06-07 20:25:18 -0400

    button function

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var dateDisplay = function(){
     var dayEl = document.getElementById(dayCurrent)
     dayEl.append(dateCurrent)  
 }
+
+$("button").on("click", function(){
+    var task = $(this).siblings("textarea").val()
+    console.log(task)
+
+    var taskTime = $(this).siblings(".timebox").attr("id")
+    console.log(taskTime)
+
+    localStorage.setItem(taskTime, task)
+
+    window.alert("Your Task is SAVED to your Scheduler!")
+})
