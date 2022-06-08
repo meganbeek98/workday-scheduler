c7d676218d58f9944cb3020c535726d8bf5b323f
meganbeek98 <meganbeek98@gmail.com>
2022-06-07 20:32:11 -0400

    siblings

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var dateDisplay = function(){
     var dateCurrent = moment().format("dddd, MMMM Do YYYY")
     var dayEl = document.getElementById(dayCurrent)
-    dayEl.append(dateCurrent)  
+    dayEl.append(dateCurrent)
 }
+
+window.addEventListener('load', dateDisplay)
 
 $("button").on("click", function(){
     var task = $(this).siblings("textarea").val()
@@ -15,3 +17,14 @@ $("button").on("click", function(){
 
     window.alert("Your Task is SAVED to your Scheduler!")
 })
+
+$("#9").siblings("textarea").val(localStorage.getItem(""))
+$("#10").siblings("textarea").val(localStorage.getItem(""))
+$("#11").siblings("textarea").val(localStorage.getItem(""))
+$("#12").siblings("textarea").val(localStorage.getItem(""))
+$("#1").siblings("textarea").val(localStorage.getItem(""))
+$("#2").siblings("textarea").val(localStorage.getItem(""))
+$("#3").siblings("textarea").val(localStorage.getItem(""))
+$("#4").siblings("textarea").val(localStorage.getItem(""))
+$("#5").siblings("textarea").val(localStorage.getItem(""))
+
